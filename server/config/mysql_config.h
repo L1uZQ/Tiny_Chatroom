fb1a0b578d8348e5c62d02fc868208b2f927c2b6
L1uZQ <lzquestc@outlook.com>
2023-08-28 20:08:02 +0800

    first commit

---
+++ server/config/mysql_config.h
@@ -0,0 +1,9 @@
+//
+// Created by cswen on 2020/8/3.
+//
+#pragma once
+#define HOST "127.0.0.1"
+#define USERNAME "root"
+#define PASSWORD "liu2461997130"
+#define PORT 3306
+#define DATABASE "ChatRoom_user"
---
+++ server/config/server_config.h
@@ -0,0 +1,9 @@
+//
+// Created by cswen on 2020/8/3.
+//
+#pragma once
+// #define HOST "43.136.177.64"
+// #define HOST "127.0.0.1"
+#define HOST "0.0.0.0"
+#define PORT 9007
+#define MAX_CONNECTIONS 1024
